c4b7d3ba994ddf3b0d0778d291fd2bf7dbac57bf
MaadixNet <root@maadix.maadix.org>
2017-04-07 20:08:20 +0200

    Added myself to Copyright headers

--- static/js/client/end.js
+++ static/js/client/end.js
@@ -1,19 +1,17 @@
-/*Author: Maadix.net - https://github.com/MaadixNet
+/* Copyright 2014 Alexander Oberegger
+ * Copyright 2017 MaadiX
+ *
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-     GNU General Public License for more details.
+http://www.apache.org/licenses/LICENSE-2.0
 
-     see <http://www.gnu.org/licenses/>.
-*/
-
-/* Scripts that need or can  be run at the end of the page
- * */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 
 var setPw = function(groupId, padName, Password){
